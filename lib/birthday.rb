b8361304bd81eeff3440db6429a8648ac6d341d3
Joshua Collins <pc.refuge@gmail.com>
2020-08-03 18:00:24 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,3 +10,9 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+
+def age_appropriate_birthday(birthday_kids)
+  if age <= 12
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
